bcaca2b50b7e1d5cdba5f54d0dec47bbfd8d8cae
MartaRodriguezOnis <marta.ronis@gmail.com>
2022-07-10 16:00:32 +0200

    Fix rest of issues, the conditionals and add new reset button

--- js/main.js
+++ js/main.js
@@ -1,32 +1,30 @@
 "use strict";
 
-//ELEMENTOS DEL HTML
+// ELEMENTOS DEL HTML
 
 const numberInput = document.querySelector(".js_numberInput");
 const testButton = document.querySelector(".js_testButton");
 const clueParagraph = document.querySelector(".js_clueParagraph");
 const attemptParagraph = document.querySelector(".js_attemptParagraph");
+const resetButton = document.querySelector(".js_resetButton");
+
+// FUNCIONES
 
 function getRandomNumber(max) {
   return Math.ceil(Math.random() * max);
 }
 
-const randomNumber = getRandomNumber(100);
-console.log(randomNumber);
-
 const writeParagraph = (message) => {
   clueParagraph.innerHTML = message;
 };
 
-// const validNumber = () => {
-//   if (valueNumber < 1 || valueNumber > 100) {
-//     clueParagraph.innerHTML = `El número debe estar entre 1 y 100`;
-//   }
-// };
-
-const showMessage = () => {
+const checkNumber = () => {
   const valueNumber = parseInt(numberInput.value);
-  if (valueNumber > randomNumber) {
+  if (isNaN(valueNumber)) {
+    writeParagraph(`¡¡¡Debes introducir un número entre 1 y 100!!!`);
+  } else if (valueNumber < 1 || valueNumber > 100) {
+    writeParagraph(`El número debe estar entre 1 y 100.`);
+  } else if (valueNumber > randomNumber) {
     writeParagraph(`Demasiado alto`);
   } else if (valueNumber < randomNumber) {
     writeParagraph(`Demasiado bajo`);
@@ -35,7 +33,6 @@ const showMessage = () => {
   }
 };
 
-let counter = 0;
 function addCounter() {
   const valueNumber = parseInt(numberInput.value);
   if (valueNumber !== randomNumber) {
@@ -47,9 +44,21 @@ function addCounter() {
 
 function handleClick(ev) {
   ev.preventDefault();
-  showMessage();
-  // validNumber();
+  checkNumber();
   addCounter();
 }
+function handleClickReset(ev) {
+  ev.preventDefault();
+  location.reload();
+}
+
+// EVENTOS
 
 testButton.addEventListener("click", handleClick);
+resetButton.addEventListener("click", handleClickReset);
+
+// CÓDIGO GLOBAL
+
+let counter = 0;
+const randomNumber = getRandomNumber(100);
+console.log(randomNumber);
